93d298082d1cec61ca3cbb29a3b1f35956526dad
Robert Snyder <r.snyder49@gmail.com>
2019-01-17 01:04:33 +0000

    Automatically backed up by Learn

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -2,5 +2,10 @@ export function managePresents(state, action){
 }
 
 function managePresents(state, action) {
-  
+  switch (action.type) {
+      case 'INCREASE':
+        return {numberOfPresents: state.numberOfPresents + 1}
+      default:
+        return state;
+  }
 }
